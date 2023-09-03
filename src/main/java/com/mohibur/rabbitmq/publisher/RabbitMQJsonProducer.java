1d49243165ff60d4df2dd0beb2578ffc03760667
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-09-04 01:11:59 +0600

    Create RabbitMQJsonProducer.java

---
+++ src/main/java/com/mohibur/rabbitmq/publisher/RabbitMQJsonProducer.java
@@ -0,0 +1,27 @@
+package com.mohibur.rabbitmq.publisher;
+
+import com.mohibur.rabbitmq.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJsonProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonProducer.class);
+    private final RabbitTemplate rabbitTemplate;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
+    public RabbitMQJsonProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendJsonMessage(User user) {
+        LOGGER.info(String.format("Json message sent : %s", user.toString()));
+        rabbitTemplate.convertAndSend(exchange, routingJsonKey, user);
+    }
+}
