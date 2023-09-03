5b60cd705d4b44a923836fcf6ccaaaa19ab8d09f
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-09-04 00:46:16 +0600

    Create RabbitMQProducer.java

---
+++ src/main/java/com/mohibur/rabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,27 @@
+package com.mohibur.rabbitmq.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+    private final RabbitTemplate rabbitTemplate;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOGGER.info(String.format("Message sent : %S", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
